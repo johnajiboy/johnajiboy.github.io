bf3c4349b663d3188791d5d20c61dd9b75b75130
johnajiboy <82177432+johnajiboy@users.noreply.github.com>
2023-06-16 11:03:57 +0100

    landing.js

--- landing.js
+++ landing.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'The Download Inspector  project is a web browser extension/application that intercepts<br> download requests from the browser and checks the requested files for malicious software using<br> an API and database. The goal of the Minimum Viable Product (MVP) is to provide basic <br>functionality for users to safely download files without worrying about potential malware infections. '; /* The text */
+var txt = 'The Download Inspector  project is a web browser extension/application that intercepts download requests from the browser and checks the requested files for malicious software using an API and database. The goal of the Minimum Viable Product (MVP) is to provide basic functionality for users to safely download files without worrying about potential malware infections. '; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
@@ -8,4 +8,12 @@ function typeWriter() {
     i++;
     setTimeout(typeWriter, speed);
   }
-}
+};
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+  sidemenu.style.right = "0";
+};
+function closemenu(){
+  sidemenu.style.right = "-200px";
+};
